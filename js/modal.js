ebec37c52140e20e3b0a0c2a44bd48f5df495829
olivertoussaint <toussaint.olivier66@gmail.com>
2022-06-29 23:35:07 +0200

    add a function to close modal

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.getElementsByClassName("close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
+closeModalBtn[0].addEventListener('click', closeModal);
